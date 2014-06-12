2003c74da8bc1fa37d07a559d7eae91cd27cf0a1
Kman25 <kevyugi@gmail.com>
2014-06-12 18:57:28 +0900

    Configured excel download properly

--- routes/excel.js
+++ routes/excel.js
@@ -1,29 +1,41 @@
 var fs = require('fs');
+var xlsx = require('node-xlsx');
 
-exports.export = function(req, res, db) {
+exports.export = function (req, res, db) {
 
-	var writeStream = fs.createWriteStream("event.xls");
-	
-	req.db.User.find({}, function(err, users) {
-		var header = "Name"+"\t"+"Company"+"\t"+"Address"+"\t"+"Phone Number"+"\t"+"Email"+"\t"+"Attendance"+"\t"+"Comment"+"\t"+"Registered Date"+"\n";
-		writeStream.write(header);
+	var fileData = {worksheets: [
+		{
+			'name':'mySheetName', 
+			'data':[
+	    		['Name', 'Company', 'Address', 'Phone Number', 'Email', 'Attendance', 'Comment', 'Registered Date']
+			]
+		}
+	]}
 
+	var users = req.db.User.find({}, function(err, users) {
 		users.forEach(function(user, index) {
-			writeStream.write(formatRow(user));
-		})
-		writeStream.end();
+			fileData.worksheets[0].data.push(formatRow(user));
+		});
+		fs.writeFile('event.xlsx', xlsx.build(fileData), function(err){if (err) throw err;});
+		res.download('./event.xlsx', 'event.xlsx', function(err) {
+			if(err) throw err;
+		});
 	});
 
-	res.download('./event.xls', 'event.xls');
 };
 
 function formatRow(user) {
 	var fields = ['name', 'company', 'address', 'phoneNumber', 'email', 'attendance', 'comment', 'date'];
-	var row = "";
+	var row = [];
 
 	for (var i = 0; i < fields.length; i++) {
-	    row += user[fields[i]] + "\t";
+		var format = 'General'; 
+
+		if (fields[i] == 'date')
+			format = 'YYYY/MM/DD'
+
+	    row.push({value: user[fields[i]], formatCode: format});
 	}
 
-	return row + "\n";
+	return row;
 }
