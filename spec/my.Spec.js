f6455ff7df1df6965cca01c54287e4672d1f11a4
Kman25 <kevyugi@gmail.com>
2014-07-22 02:46:23 +0900

    Added route tests

--- spec/my.Spec.js
+++ spec/my.Spec.js
@@ -1,11 +1,32 @@
-describe('app', function() {
+describe('routes', function() {
 
 var request = require('request');
 
-	it ('should respond to /', function(done) {
+	it ('should respond when you perform a GET on the / route', function(done) {
 		request('http://localhost:8080/', function(error, response, body) {
 			expect(response.statusCode).toBe(200);
 			done();
 		});
-	})	
-})
+	});	
+
+	it ('should respond to when you perform a GET on the /users route', function(done) {
+		request('http://localhost:8080/users', function(error, response, body) {
+			expect(response.statusCode).toBe(200);
+			done();
+		});
+	});
+
+	it ('should respond to when you perform a POST on the /users route', function(done) {
+		request.post('http://localhost:8080/users', function(error, response, body) {
+			expect(response.statusCode).toBe(200);
+			done();
+		});
+	});
+
+	it ('should respond to when you perform GET on the /export route', function(done) {
+		request('http://localhost:8080/export', function(error, response, body) {
+			expect(response.statusCode).toBe(200);
+			done();
+		});
+	});
+});
